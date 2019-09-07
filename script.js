240e511ce5fe9bfc2184f77dc855b289d8bd084c
Sparkzmaster <55006735+Sparkzmaster@users.noreply.github.com>
2019-09-07 20:04:02 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function CalculateEnergy(){
 	var totalEnergy = document.getElementById("energytotal").value;
-	var totalKg = totalEnergy * 0.155 * 0.28307;
+	var totalKg = totalEnergy * 0.161 * 0.28307;
 	var motorBikes = totalKg * 10;
+	document.getElementById("pBikes").innerHTML = motorBikes + " km.";
+}
+function aSelectFunction(){
+document.getElementById("aMagicSelect").style.display = "none";
+document.getElementById("bMagicSelect").style.display = "none";
+document.getElementById("cMagicSelect").style.display = "none";
+document.getElementById("dMagicSelect").style.display = "none";
+
+text = document.getElementById("aSelect").value;
+document.getElementById(text).style.display = "block";
+
 }
